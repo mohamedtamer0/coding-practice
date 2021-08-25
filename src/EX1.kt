14a2efa6828bea9c518812bf23f5e36743901aed
mohamedtamer0 <mt4452242@gmail.com>
2021-08-25 04:20:45 +0200

    update

--- src/EX1.kt
+++ src/EX1.kt
@@ -8,6 +8,16 @@ fun sayHello(){
 
 fun sayHello2(greeting: String, item:String) = println("$greeting $item")
 
+
+
+
+fun sayHello2(greeting: String , vararg itemToGreet:String) {
+    itemToGreet.forEach {
+            itemToGreet ->
+        println("$greeting $itemToGreet")
+    }
+}
+
 fun main(args: Array<String>) {
     sayHello()
     sayHello2("Hello","Tamer")
@@ -29,5 +39,41 @@ fun main(args: Array<String>) {
         println(interestingThings)
     }
 
+    interestingThings.forEachIndexed{
+        index, interestingThing ->
+        println("$interestingThing is at index $index")
+    }
 
+
+
+    println("===========================")
+
+    val map = mapOf(1 to "a", 2 to "b")
+    map.forEach { key, value -> println("$key -> $value") }
+
+
+    println("===========================")
+
+
+    sayHello2("Hi","Kotlin", "Java","HTML")
+
+
+    val entity = Entity.Factory.create()
+    Entity.id
+}
+
+class Entity private constructor(val id :String)
+{
+    companion object Factory : idProv {
+        const val id = "id"
+        fun create() = Entity("id")
+        override fun getId(): String {
+            return "123"
+        }
+    }
+
+}
+
+interface idProv {
+    fun getId() :String
 }
